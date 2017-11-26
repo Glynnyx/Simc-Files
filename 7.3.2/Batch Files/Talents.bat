85a2ebe1baf4da14ee5f7d47fbadc00d6e988afb
Glynnyx <glynny_battle.net@hotmail.co.uk>
2017-11-26 10:43:42 +0000

    Talents done?

---
+++ 7.3.2/Batch Files/Talents.bat
@@ -0,0 +1,14 @@
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Talents\T21_Mage_Frost_Talents_Single.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Talents\T21_Mage_Frost_Talents_Four.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Talents\T21_Mage_Frost_Talents_Two.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\2. Fire\Simc Files\Talents\T21_Mage_Fire_Talents_Single"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\2. Fire\Simc Files\Talents\T21_Mage_Fire_Talents_Two"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\2. Fire\Simc Files\Talents\T21_Mage_Fire_Talents_Four"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\2. Arcane\Simc Files\Talents\T21_Mage_Arcane_Talents_Single"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\2. Arcane\Simc Files\Talents\T21_Mage_Arcane_Talents_Two"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\2. Arcane\Simc Files\Talents\T21_Mage_Arcane_Talents_Four"
+
+
+
+
+
