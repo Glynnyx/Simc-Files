802a32152410337c8e4d9f570d288351b0aa74f0
Glynnyx <glynny_battle.net@hotmail.co.uk>
2017-11-28 08:46:44 +0000

    More things!

---
+++ 7.3.2/Batch Files/Neck_Enchants.bat
@@ -0,0 +1,3 @@
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Neck Enchants\T21_Mage_Frost_Neck_Enchants.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\2. Fire\Simc Files\Neck Enchants\T21_Mage_Fire_Neck_Enchants.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\3. Arcane\Simc Files\Neck Enchants\T21_Mage_Arcane_Neck_Enchants.simc"
