c03d32a58865b2c128b0b7e1314458bd101cc11a
Glynnyx <glynny_battle.net@hotmail.co.uk>
2017-11-28 09:07:23 +0000

    More Things

---
+++ 7.3.2/Batch Files/Relics.bat
@@ -0,0 +1,3 @@
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Relics\T21_Mage_Frost_Relics.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\2. Fire\Simc Files\Relics\T21_Mage_Fire_Relics.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\3. Arcane\Simc Files\Relics\T21_Mage_Arcane_Relics.simc"
