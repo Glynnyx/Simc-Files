664c0beaf95ef9b3ba5277332572422a39685e96
Glynnyx <glynny_battle.net@hotmail.co.uk>
2017-12-02 22:13:35 +0000

    Changes

---
+++ 7.3.2/Batch Files/Relics_Frost.bat
@@ -0,0 +1,3 @@
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Relics\T21_Mage_Frost_Relics_1_Target.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Relics\T21_Mage_Frost_Relics_2_Target.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Relics\T21_Mage_Frost_Relics_4_Target.simc"
---
+++ 7.3.2/Batch Files/Talents_Frost.bat
@@ -0,0 +1,3 @@
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Talents\T21_Mage_Frost_Talents_Single.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Talents\T21_Mage_Frost_Talents_Four.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\1. Frost\Simc Files\Talents\T21_Mage_Frost_Talents_Two.simc"
