f94be5990878623e8b6ffa4fe804ebf195455e14
Glynnyx <glynny_battle.net@hotmail.co.uk>
2018-02-10 17:14:41 +0000

    [All] New Races

--- 7.3.2/Batch Files/Arcane/Races_Arcane.bat
+++ 7.3.2/Batch Files/Arcane/Races_Arcane.bat
@@ -1,2 +1,1 @@
-start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\2. Fire\Simc Files\Racials\T21_Mage_Fire_Racials.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\simc-730-03-win64" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\3. Arcane\Simc Files\Racials\T21_Mage_Arcane_Racials.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc\" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\7.3.2\3. Arcane\Simc Files\Racials\T21_Mage_Arcane_Racials.simc"
