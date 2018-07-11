a5f1ccb413ba12288dd53cd8ff96b48a18353bd7
Glynn <glynnm@formfill.co.uk>
2018-07-11 14:00:19 +0100

    Updated from 8.0 to PreRaid
    As the profiles are set to 110.

--- 03. Batch Files/All sims.bat
+++ 03. Batch Files/All sims.bat
@@ -13,7 +13,9 @@ start "simc" /d "C:\Users\glynnm\Desktop\git\simc" /wait simc.exe "C:\Users\glyn
 
 rem Start of 03. Stat Weight Sims
 
-
+start "simc" /d "C:\Users\glynnm\Desktop\git\simc" /wait simc.exe "C:\Users\glynnm\Desktop\git\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\03. Stat Weights\01. Single_Target_Stat_Weights.simc"
+start "simc" /d "C:\Users\glynnm\Desktop\git\simc" /wait simc.exe "C:\Users\glynnm\Desktop\git\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\03. Stat Weights\02. Two_Target_Stat_Weights.simc"
+start "simc" /d "C:\Users\glynnm\Desktop\git\simc" /wait simc.exe "C:\Users\glynnm\Desktop\git\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\03. Stat Weights\03. Four_Target_Stat_Weights.simc"
 
 rem Start of 04. Azerite Trait Sims
 
@@ -25,6 +27,10 @@ rem Start of 05. Neck Enchant Sims
 rem Start of 06. Stats Sims
 
 start "simc" /d "C:\Users\glynnm\Desktop\git\simc" /wait simc.exe "C:\Users\glynnm\Desktop\git\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\06. Stats\01. Single_Target_Stats_Plot_Crit.simc"
+start "simc" /d "C:\Users\glynnm\Desktop\git\simc" /wait simc.exe "C:\Users\glynnm\Desktop\git\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\06. Stats\02. Single_Target_Stats_Plot_Haste.simc"
+start "simc" /d "C:\Users\glynnm\Desktop\git\simc" /wait simc.exe "C:\Users\glynnm\Desktop\git\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\06. Stats\03. Single_Target_Stats_Plot_Mastery.simc"
+start "simc" /d "C:\Users\glynnm\Desktop\git\simc" /wait simc.exe "C:\Users\glynnm\Desktop\git\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\06. Stats\04. Single_Target_Stats_Plot_Vers.simc"
+start "simc" /d "C:\Users\glynnm\Desktop\git\simc" /wait simc.exe "C:\Users\glynnm\Desktop\git\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\06. Stats\05. Single_Target_Stats_Plot_All.simc"
 
 
 rem Start of 07. Weapons Enchants
