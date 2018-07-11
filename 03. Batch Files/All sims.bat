30078b46dc03463621e2d2195fde7223c24437d7
Glynnyx <glynny_battle.net@hotmail.co.uk>
2018-07-11 07:34:39 +0100

    [start]

---
+++ 03. Batch Files/All sims.bat
@@ -0,0 +1,25 @@
+rem Start of 01. Talents Sims 
+
+
+
+rem Start of 02. Racial Sims
+
+
+
+rem Start of 03. Stat Weight Sims
+
+
+
+rem Start of 04. Azerite Trait Sims
+
+
+
+rem Start of 05. Neck Enchant Sims
+
+
+
+rem Start of 06. Weapon Enchant Sims
+
+
+
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\01. Talents\01. Single_Target_Talents.simc"
