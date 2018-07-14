15aedf7fecfb73913c88fd5ff8918ef5c4687b8c
Glynnyx <glynny_battle.net@hotmail.co.uk>
2018-07-14 06:54:28 +0100

    prepatch update

--- 03. Batch Files/All sims.bat
+++ 03. Batch Files/All sims.bat
@@ -1,8 +1,8 @@
 rem **Start of 01. Talents Sims**
 rem Frost
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\01. Talents\01. Single_Target_Talents.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\01. Talents\02. Four_Target_Talents.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\01. Talents\03. Four_Target_Talents.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\01. Talents\01. Single_Target_Talents.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\01. Talents\02. Four_Target_Talents.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\01. Talents\03. Four_Target_Talents.simc"
 
 rem Fire
 start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\02. Fire\Simc Files\01. Talents\01. Single_Target_Talents.simc"
@@ -11,9 +11,9 @@ start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\User
 
 rem **Start of 02. Racial Sims**
 rem Frost
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\02. Racials\01. Single_Target_Racials.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\02. Racials\02. Two_Target_Racials.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\02. Racials\03. Four_Target_Racials.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\02. Racials\01. Single_Target_Racials.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\02. Racials\02. Two_Target_Racials.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\02. Racials\03. Four_Target_Racials.simc"
 
 rem Fire
 start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\02. Fire\Simc Files\02. Racials\01. Single_Target_Racials.simc"
@@ -22,9 +22,9 @@ start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\User
 
 rem **Start of 03. Stat Weight Sims**
 rem Frost
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\03. Stat Weights\01. Single_Target_Stat_Weights.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\03. Stat Weights\02. Two_Target_Stat_Weights.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\03. Stat Weights\03. Four_Target_Stat_Weights.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\03. Stat Weights\01. Single_Target_Stat_Weights.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\03. Stat Weights\02. Two_Target_Stat_Weights.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\03. Stat Weights\03. Four_Target_Stat_Weights.simc"
 
 rem **Start of 04. Azerite Trait Sims**
 rem Frost
@@ -39,11 +39,11 @@ start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\User
 
 rem **Start of 06. Stats Sims**
 rem Frost
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\06. Stats\01. Single_Target_Stats_Plot_Crit.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\06. Stats\02. Single_Target_Stats_Plot_Haste.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\06. Stats\03. Single_Target_Stats_Plot_Mastery.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\06. Stats\04. Single_Target_Stats_Plot_Vers.simc"
-start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\8.0\Mage\01. Frost\Simc Files\06. Stats\05. Single_Target_Stats_Plot_All.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\06. Stats\01. Single_Target_Stats_Plot_Crit.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\06. Stats\02. Single_Target_Stats_Plot_Haste.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\06. Stats\03. Single_Target_Stats_Plot_Mastery.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\06. Stats\04. Single_Target_Stats_Plot_Vers.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\01. Frost\Simc Files\06. Stats\05. Single_Target_Stats_Plot_All.simc"
 
 
 rem **Start of 07. Weapons Enchants**
