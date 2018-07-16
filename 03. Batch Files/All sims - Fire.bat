de0703018e5b51bbf218f7070aac187c45e64078
Glynnyx <glynny_battle.net@hotmail.co.uk>
2018-07-16 16:16:32 +0100

    Frost is done.

---
+++ 03. Batch Files/All sims - Fire.bat
@@ -0,0 +1,27 @@
+rem **Start of 01. Talents Sims**
+rem Fire
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\02. Fire\Simc Files\01. Talents\01. Single_Target_Racials.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\02. Fire\Simc Files\01. Talents\02. Two_Target_Racials.simc"
+start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe "C:\Users\Glynny-PC\Desktop\Github\Simc-Files\02. Battle For Azeroth\PreRaid\Mage\02. Fire\Simc Files\01. Talents\03. Four_Target_Racials.simc"
+
+
+rem **Start of 02. Racial Sims**
+rem Fire
+
+rem **Start of 03. Stat Weight Sims**
+rem Fire
+
+rem **Start of 04. Legendary Items**
+rem Fire
+
+
+
+rem **Start of 05. Neck Enchant Sims**
+rem Fire
+
+
+rem **Start of 06. Stats Sims**
+rem Fire
+
+
+rem This is a placeholder to copy/paste: start "simc" /d "C:\Users\Glynny-PC\Desktop\Github\simc" /wait simc.exe ""
